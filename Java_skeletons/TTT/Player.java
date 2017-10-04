e035280ee0a41fae45c984a91b96b2bcd97b8200
BuildTools <unconfigured@null.spigotmc.org>
2017-10-04 14:41:15 +0200

    asd

--- Java_skeletons/TTT/Player.java
+++ Java_skeletons/TTT/Player.java
@@ -11,6 +11,9 @@ public class Player {
      *            time before which we must have returned
      * @return the next state the board is in after our move
      */
+
+     private int index = 0;
+
     public GameState play(final GameState gameState, final Deadline deadline) {
         Vector<GameState> nextStates = new Vector<GameState>();
         gameState.findPossibleMoves(nextStates);
@@ -27,9 +30,18 @@ public class Player {
         //  nextStates.elementAt(random.nextInt(nextStates.size()));
         int alpha = -Integer.MAX_VALUE;
         int beta = Integer.MAX_VALUE;
-        return nextStates.elementAt(minmax(gameState, nextStates, 4, alpha, beta, gameState.getNextPlayer()));
+
+        int depth = 4;
+        index = 0;
+
+        //Random random = new Random();
+       // return nextStates.elementAt(random.nextInt(nextStates.size()));
+
+       int val = minmax(gameState, nextStates, depth, alpha, beta, gameState.getNextPlayer());
+       return nextStates.elementAt(index);
+
     }
-    
+
     public int minmax(GameState gameState, Vector<GameState> nextStates, int depth, int alpha, int beta, int player) {
         //Random random = new Random();
        // return nextStates.elementAt(random.nextInt(nextStates.size()));
@@ -38,11 +50,14 @@ public class Player {
         if(depth == 0 || nextStates.isEmpty()) {
             v = eval(gameState, player);
             //player a
-        } else if (player == 0) {
+        } else if (player == 1) {
             v = -Integer.MAX_VALUE;
             for(GameState g : nextStates) {
-                v = Math.max(v, minmax(g, nextStates, depth-1, alpha, beta, 1));
-                alpha = Math.max(alpha, v);
+                v = Math.max(v, minmax(g, nextStates, depth-1, alpha, beta, 2));
+                if(v > alpha) {
+                  alpha = v;
+                  index = nextStates.indexOf(g);
+                }
                 if(beta <= alpha)
                     break;
             }
@@ -50,30 +65,53 @@ public class Player {
         } else {
             v = Integer.MAX_VALUE;
             for(GameState g : nextStates) {
-                v = Math.min(v, minmax(g, nextStates, depth-1, alpha, beta, 0));
-                beta = Math.min(beta, v);
-                if(beta <= alpha) 
+              v = Math.min(v, minmax(g, nextStates, depth-1, alpha, beta, 1));
+              if(v < beta) {
+                beta = v;
+                index = nextStates.indexOf(g);
+              }
+                if(beta <= alpha)
                     break;
             }
-        } 
+        }
         return v;
-
     }
 
     public int eval(GameState gameState, int player) {
-        
         final int n = gameState.BOARD_SIZE;
-        
-        int row = 0, col = 0, diag = 0;
+        int[] combinations = new int[10];
+
         // check rows
         for(int i = 0; i < n; i++) {
+          int power = 1;
             for(int j = 0; j < n; j++) {
-                row += (gameState.at(i, j) == player) ? 1 : 0;
-                col += (gameState.at(j, i) == player) ? 1 : 0;
+                combinations[i] += checkCell(gameState, player, i, j);
+                combinations[4 + i] += checkCell(gameState, player, j, i);
             }
-            diag += (gameState.at(i, i) == player) ? 1 : 0;
-            diag += (gameState.at(n-i, n-i) == player) ? 1 : 0;
+            combinations[8] += checkCell(gameState, player, i, i);
+            combinations[9] += checkCell(gameState, player, n - i, n - i);
         }
-        return row + col + diag;
+        int max = 0;
+        //System.err.println("New state");
+        for(int i : combinations) {
+          //System.err.println("Comb: " + i);
+          if(i > max) {
+            max = i;
+          }
+        }
+        //return Math.max(Math.max(row, col), Math.max(row, diag));
+        return max;
+    }
+
+    private int checkCell(GameState gameState, int player, int i, int j) {
+      if(gameState.at(i, j) == player) {
+        return 10;
+      }
+      else if(gameState.at(i, j) == 0) {
+        return 0;
+      }
+      else {
+        return -10;
+      }
     }
 }

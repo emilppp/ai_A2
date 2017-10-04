ecaa0ae47fe76288aa95a86d66e49f2a7d03275e
Emil Persson <emiper@kth.se>
2017-10-04 10:40:34 +0200

    hehe

--- Java_skeletons/TTT/Player.java
+++ Java_skeletons/TTT/Player.java
@@ -25,31 +25,31 @@ public class Player {
          * the best next state. This skeleton returns a random move instead.
          */
         //  nextStates.elementAt(random.nextInt(nextStates.size()));
-        int alpha = -INTEGER.MAX_VALUE;
-        int beta = INTEGER.MAX_VALUE;
-        return minmax(gameState, nextStates, 4, alpha, beta, gameState.getNextPlayer());
+        int alpha = -Integer.MAX_VALUE;
+        int beta = Integer.MAX_VALUE;
+        return nextStates.elementAt(minmax(gameState, nextStates, 4, alpha, beta, gameState.getNextPlayer()));
     }
     
     public int minmax(GameState gameState, Vector<GameState> nextStates, int depth, int alpha, int beta, int player) {
-        Random random = new Random();
-        return nextStates.elementAt(random.nextInt(nextStates.size()));
+        //Random random = new Random();
+       // return nextStates.elementAt(random.nextInt(nextStates.size()));
         int v = 0;
 
         if(depth == 0 || nextStates.isEmpty()) {
             v = eval(gameState, player);
             //player a
         } else if (player == 0) {
-            v = -INTEGER.MAX_VALUE;
-            for(gameState g : nextStates) {
+            v = -Integer.MAX_VALUE;
+            for(GameState g : nextStates) {
                 v = Math.max(v, minmax(g, nextStates, depth-1, alpha, beta, 1));
-                alpha = max(alpha, v);
+                alpha = Math.max(alpha, v);
                 if(beta <= alpha)
                     break;
             }
             //player b
         } else {
-            v = INTEGER.MAX_VALUE;
-            for(gameState g : nextStates) {
+            v = Integer.MAX_VALUE;
+            for(GameState g : nextStates) {
                 v = Math.min(v, minmax(g, nextStates, depth-1, alpha, beta, 0));
                 beta = Math.min(beta, v);
                 if(beta <= alpha) 
@@ -68,11 +68,11 @@ public class Player {
         // check rows
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                row += (gameState.at(i, j) & player) ? 1 : 0;
-                col += (gameState.at(j, i) & player) ? 1 : 0;
+                row += (gameState.at(i, j) == player) ? 1 : 0;
+                col += (gameState.at(j, i) == player) ? 1 : 0;
             }
-            diag += (gameState(i, i) & player) ? 1 : 0;
-            diag += (gameState(n-i, n-i) & player) ? 1 : 0;
+            diag += (gameState.at(i, i) == player) ? 1 : 0;
+            diag += (gameState.at(n-i, n-i) == player) ? 1 : 0;
         }
         return row + col + diag;
     }
